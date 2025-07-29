5341c500591e9577df5e8ee07c1b19c5a00a97d9
gjpin <3874515+gjpin@users.noreply.github.com>
2025-07-29 21:25:27 +0100

    update firefox config

--- configs/firefox/user.js
+++ configs/firefox/user.js
@@ -205,25 +205,6 @@ user_pref("findbar.highlightAll", true);
 user_pref("layout.word_select.eat_space_to_next_word", false);
 
 /****************************************************************************
- * SECTION: SMOOTHFOX                                                       *
- ****************************************************************************/
-// https://github.com/yokoffing/Betterfox/blob/master/Smoothfox.js
-// NATURAL SMOOTH SCROLLING V3 [MODIFIED]
-
-user_pref("apz.overscroll.enabled", true); // DEFAULT NON-LINUX
-user_pref("general.smoothScroll", true); // DEFAULT
-user_pref("general.smoothScroll.msdPhysics.continuousMotionMaxDeltaMS", 12);
-user_pref("general.smoothScroll.msdPhysics.enabled", true);
-user_pref("general.smoothScroll.msdPhysics.motionBeginSpringConstant", 600);
-user_pref("general.smoothScroll.msdPhysics.regularSpringConstant", 650);
-user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaMS", 25);
-user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaRatio", "2");
-user_pref("general.smoothScroll.msdPhysics.slowdownSpringConstant", 250);
-user_pref("general.smoothScroll.currentVelocityWeighting", "1");
-user_pref("general.smoothScroll.stopDecelerationWeighting", "1");
-user_pref("mousewheel.default.delta_multiplier_y", 300); // 250-400; adjust this number to your liking
-
-/****************************************************************************
  * START: MY OVERRIDES                                                      *
  ****************************************************************************/
 // visit https://github.com/yokoffing/Betterfox/wiki/Common-Overrides
