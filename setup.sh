7d22a811f8158fa6bd059edf9dc7319919406a52
gjpin <3874515+gjpin@users.noreply.github.com>
2026-03-22 18:00:31 +0000

    add new model

--- setup.sh
+++ setup.sh
@@ -552,17 +552,26 @@ hf download \
 --include "gpt-oss-20b-F16.gguf" \
 --local-dir "$HOME/llm"
 
+# Download Qwen3.5 35B A3B
+# https://huggingface.co/mradermacher/Qwen3.5-35B-A3B-heretic-Opus-4.6-Distilled-i1-GGUF
+hf download \
+"mradermacher/Qwen3.5-35B-A3B-heretic-Opus-4.6-Distilled-i1-GGUF" \
+--include "Qwen3.5-35B-A3B-heretic-Opus-4.6-Distilled.i1-Q4_K_M.gguf" \
+--local-dir "$HOME/llm"
+
 # Configure aliases
 tee ${HOME}/.zshrc.d/llm << 'EOF'
-LLAMA_COMMON="--threads 8 --n-gpu-layers 99 --jinja"
+LLAMA_COMMON="--threads 8 --threads-batch 8 --n-gpu-layers 99 --jinja --batch-size 4096 --ubatch-size 2048 --cache-type-k q8_0 --cache-type-v q8_0"
 
 alias devstral="llama-server $LLAMA_COMMON \
   --model \$HOME/llm/Devstral-Small-2-24B-Instruct-2512-UD-Q4_K_XL.gguf \
+  --alias devstral \
   --ctx-size 65536 \
   --temp 0.15 --min_p 0.01"
 
 alias qwen3.5-9b="llama-server $LLAMA_COMMON \
   --model \$HOME/llm/Qwen3.5-9B-Q5_K_M.gguf \
+  --alias qwen3.5-9b \
   --ctx-size 65536 \
   --temp 1.0 --min-p 0.0 --top-p 0.95 --top-k 20 \
   --repeat-penalty 1.00 --presence-penalty 1.5 \
@@ -571,6 +580,16 @@ alias qwen3.5-9b="llama-server $LLAMA_COMMON \
 
 alias qwen3.5-35b-a3b="llama-server $LLAMA_COMMON \
   --model \$HOME/llm/Qwen3.5-35B-A3B-Q4_K_M.gguf \
+  --alias qwen3.5-35b-a3b \
+  --ctx-size 65536 \
+  --temp 0.6 --min-p 0.0 --top-p 0.95 --top-k 20 \
+  --repeat-penalty 1.00 --presence-penalty 0.0 \
+  --chat-template-kwargs '{\"enable_thinking\":true}' \
+  --flash-attn on"
+
+alias qwen3.5-35b-a3b-opus="llama-server $LLAMA_COMMON \
+  --model \$HOME/llm/Qwen3.5-35B-A3B-heretic-Opus-4.6-Distilled.i1-Q4_K_M.gguf \
+  --alias qwen3.5-35b-a3b-opus \
   --ctx-size 65536 \
   --temp 0.6 --min-p 0.0 --top-p 0.95 --top-k 20 \
   --repeat-penalty 1.00 --presence-penalty 0.0 \
@@ -579,6 +598,7 @@ alias qwen3.5-35b-a3b="llama-server $LLAMA_COMMON \
 
 alias omnicoder="llama-server $LLAMA_COMMON \
   --model \$HOME/llm/omnicoder-9b-q5_k_m.gguf \
+  --alias omnicoder \
   --ctx-size 65536 \
   --temp 0.4 --min-p 0.01 --top-p 0.95 --top-k 20 \
   --presence-penalty 0.0 \
@@ -587,6 +607,7 @@ alias omnicoder="llama-server $LLAMA_COMMON \
 
 alias glm-4.7-flash="llama-server $LLAMA_COMMON \
   --model \$HOME/llm/GLM-4.7-Flash-Q4_K_M.gguf \
+  --alias gml-4.7-flash \
   --ctx-size 65536 \
   --temp 0.7 --min-p 0.01 --top-p 1.0 \
   --repeat-penalty 1.00 \
@@ -594,6 +615,7 @@ alias glm-4.7-flash="llama-server $LLAMA_COMMON \
 
 alias gpt-oss-20b="llama-server $LLAMA_COMMON \
   --model \$HOME/llm/gpt-oss-20b-F16.gguf \
+  --alias gpt-oss-20b \
   --ctx-size 65536 \
   --temp 1.0 --top-p 1.0 --top-k 0 \
   --flash-attn on"
